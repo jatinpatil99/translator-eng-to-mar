51e9659c46391b7228d8580affa84a60a709449d
jatinpatil99 <43403515+jatinpatil99@users.noreply.github.com>
2021-05-21 19:39:44 +0530

    Update app.py

--- app.py
+++ app.py
@@ -6,7 +6,7 @@ app = Flask(__name__)
 
 @app.route('/', methods=['GET','POST'])
 def home():
-    return render_template('index.html')
+    return render_template('Index.html')
 
 @app.route('/submit', methods=['GET','POST'])
 def submit():
